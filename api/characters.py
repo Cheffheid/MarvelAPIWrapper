998c6800716b7a8412cf59711b439efa19b81393
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2014-08-03 20:32:54 -0400

    Additional request requirements
    Hash, timestamp, private key.

--- api/characters.py
+++ api/characters.py
@@ -2,9 +2,10 @@ from parent import MarvelParent
 
 class MarvelCharacters(MarvelParent):
 
-	def __init__(self, apikey, base_url):
+	def __init__(self, apikey, privkey, base_url):
 		self.API_URL = base_url + '/characters'
 		self.API_KEY = apikey
+		self.PRIV_KEY = privkey
 
 	def getCharacters(self, id, args):
 		return "Not a valid method for Characters, use getOne or getList instead"
--- marvel_api.py
+++ marvel_api.py
@@ -6,12 +6,13 @@ from api.creators import MarvelCreators
 from api.stories import MarvelStories
 
 class MarvelAPIObject(object):
-	BASE_URL = "http://gateway.marvel.com:80/v1/public"
+	BASE_URL = "http://gateway.marvel.com/v1/public"
 	
-	def __init__(self, apikey=None):
+	def __init__(self, apikey=None, privkey=None):
 		self.API_KEY = apikey
+		self.PRIV_KEY = privkey
 
-		self.characters = MarvelCharacters(self.API_KEY, self.BASE_URL)
+		self.characters = MarvelCharacters(self.API_KEY, self.PRIV_KEY, self.BASE_URL)
 		self.comics = MarvelComics(self.API_KEY, self.BASE_URL)
 		self.series = MarvelSeries(self.API_KEY, self.BASE_URL)
 		self.events = MarvelEvents(self.API_KEY, self.BASE_URL)
